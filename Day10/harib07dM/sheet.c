913c18c5097382fb205269b1f5c51b3640d7efa9
No000 <no000.hima@gmail.com>
2019-12-19 23:36:48 +0900

    高速化完了

--- Day10/harib07dM/sheet.c
+++ Day10/harib07dM/sheet.c
@@ -62,13 +62,24 @@ void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1)
     sht = ctl->sheets[h];
     buf = sht->buf;
     /* vx0~vy1を使って、bx0~by1を逆算する */
-    bx0 = vx0 - sht->vx0; /* 全体のx座標の大きさから、書き換える場所を引く */
+    bx0 = vx0 - sht->vx0; /* bx0のデータがvx0の値に依存する */
     by0 = vy0 - sht->vy0;
     bx1 = vx1 - sht->vx0;
     by1 = vy1 - sht->vy0;
-    if (bx0 < 0) { bx0 = 0; }
-    if (by0 < 0) { by0 = 0; }
-    
+    if (bx0 < 0) { bx0 = 0; }   /* 右下に小さい重ね合わせ処理が来た場合 */
+    if (by0 < 0) { by0 = 0; }   /*  右下に小さい重ね合わせ処理が来た場合*/
+    if (bx1 > sht->bxsize) { bx1 = sht->bxsize; }
+    if (by1 > sht->bysize) { by1 = sht->bysize; }
+    for (by = by0; by < by1; by++) {
+      vy = sht->vy0 + by;
+      for (bx = bx0; bx < bx1; bx++) {
+        vx = sht->vx0 + bx;
+        c = buf[by * sht->bxsize + bx];
+        if (c != sht->col_inv) {
+          vram[vy * ctl->xsize + vx] = c;
+        }
+      }
+    }
   }
   return;
 }
