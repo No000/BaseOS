30435266ecfbefc33a11797d053ca9a69cd88641
No000 <no000.hima@gmail.com>
2019-12-28 17:27:50 +0900

    複数のタイマを

--- Day12/harin09dM/bootpack.h
+++ Day12/harin09dM/bootpack.h
@@ -179,13 +179,20 @@ void sheet_slide(struct SHEET *sht, int vx0, int vy0);
 void sheet_free(struct SHEET *sht);
 
 /* timer.c */
+#define MAX_TIMER   500
+struct TIMER {
+    unsigned int timeout, flags;
+    struct FIFO8 *fifo;
+    unsigned char data;
+};
 struct TIMERCTL {
     unsigned int count;    /* カウンタ */
-    unsigned int timeout;  /* 残り時間 */
-    struct FIFO8 *fifo;    /* バッファ */
-    unsigned char data;    /* 送信用データ */
+    struct TIMER timer[MAX_TIMER];
 };
 extern struct TIMERCTL timerctl;
 void init_pit(void);
+struct TIMER *timer_alloc(void);
+void timer_free(struct TIMER *timer);
+void timer_init(struct TIMER *timer, struct FIFO8 *fifo, unsigned char data);
+void timer_settime(struct TIMER *timer, unsigned int timeout);
 void inthandler20(int *esp);
-void settimer(unsigned int timeout, struct FIFO8 *fifo, unsigned char data);
--- Day12/harin09dM/timer.c
+++ Day12/harin09dM/timer.c
@@ -7,42 +7,70 @@
 
 struct TIMERCTL timerctl; /* タイマー管理用 */
 
+#define TIMER_FLAGS_ALLOC   1   /* 確保した状態 */
+#define TIMER_FLAGS_USING   2   /* タイマ作動中 */
+
 /* IRQ0の割り込み処理変更 */
 void init_pit(void)
 {
+  int i;
   io_out8(PIT_CTRL, 0x34);  
   /* 2進数カウント、レートジェネレータ、16bitリードロード（下位8bit、上位8bitの順）、カウンタ0 */
   io_out8(PIT_CNT0, 0x9c);  /* 割り込み周期の下位8bit */
   io_out8(PIT_CNT0, 0x2e);  /* 割り込み周期の上位8bit */
   timerctl.count = 0;
-  timerctl.timeout = 0;
+  for (i = 0; i < MAX_TIMER; i++) {
+    timerctl.timer[i].flags = 0;
+  }
+  return;
+}
+
+struct TIMER *timer_alloc(void)
+{
+  int i;
+  for (i = 0; i < MAX_TIMER; i++) {
+    if ( timerctl.timer[i].flags == 0) {
+      timerctl.timer[i].flags = TIMER_FLAGS_ALLOC;
+      return &timerctl.timer[i];
+    }
+  }
+  return 0; /* 見つからなかった */
+}
+
+void timer_free(struct TIMER *timer)
+{
+  timer->flags = 0;  /* 未使用 */
+  return;
+}
+
+void timer_init(struct TIMER *timer, struct FIFO8 *fifo, unsigned char data)
+{
+  timer->fifo = fifo;
+  timer->data = data;
+  return;
+}
+
+void timer_settime(struct TIMER *timer, unsigned int timeout)
+{
+  timer->timeout = timeout;
+  timer->flags = TIMER_FLAGS_USING;
   return;
 }
 
 /* IDT20の処理設定 */
 void inthandler20(int *esp)
 {
+  int i;
   io_out8(PIC0_OCW2, 0x60); /* IRQ-00受付完了をPICに通知 */
   timerctl.count++;   /* 指定に従いカウントする */
-  if (timerctl.timeout > 0) {
-    timerctl.timeout--;
-    if (timerctl.timeout == 0) {
-      fifo8_put(timerctl.fifo, timerctl.data);  
-      /* カウントが0になったらFIFOバッファに送る */
+  for (i = 0; i < MAX_TIMER; i++) {
+    if (timerctl.timer[i].flags == TIMER_FLAGS_USING) {
+      timerctl.timer[i].timeout--;
+      if (timerctl.timer[i].timeout == 0) {
+        timerctl.timer[i].flags = TIMER_FLAGS_ALLOC;
+        fifo8_put(timerctl.timer[i].fifo, timerctl.timer[i].data);
+      }
     }
   }
-  return;
-}
-
-void settimer(unsigned int timeout, struct FIFO8 *fifo, unsigned char data)
-{
-  int eflags;
-  eflags = io_load_eflags();  /* フラグレジスタの退避 */
-  io_cli();     /* 割り込み禁止 */
-  timerctl.timeout = timeout;
-  timerctl.fifo = fifo;
-  timerctl.data = data;
-  io_store_eflags(eflags);    
-  /* フラグレジスタを戻す(この際IFフラグも戻る) */
   return;
 }
