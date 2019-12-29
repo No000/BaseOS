fe44060b24ef6e0495a216183be25ebaee43a7e6
No000 <no000.hima@gmail.com>
2019-12-29 15:03:01 +0900

    FIFOバッファを見直す（１）

--- Day13/harib10bM/bootpack.c
+++ Day13/harib10bM/bootpack.c
@@ -10,7 +10,7 @@ void HariMain(void)
 {
     struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
     struct FIFO8 timerfifo, timerfifo2, timerfifo3;
-    char s[40], keybuf[32], mousebuf[128], timerbuf[8], timerbuf2[8], timerbuf3[8]; /* 各種バッファ */
+    char s[40], keybuf[32], mousebuf[128], timerbuf[8]; /* 各種バッファ */
     struct TIMER *timer, *timer2, *timer3;
     int mx, my, i;
     unsigned int memtotal;
@@ -31,15 +31,13 @@ void HariMain(void)
 
     fifo8_init(&timerfifo, 8, timerbuf);
     timer = timer_alloc();
-    timer_init(timer, &timerfifo, 1);
+    timer_init(timer, &timerfifo, 10);
     timer_settime(timer, 1000);
-    fifo8_init(&timerfifo2, 8, timerbuf2);
     timer2 = timer_alloc();
-    timer_init(timer2, &timerfifo2, 1);
+    timer_init(timer2, &timerfifo, 3);
     timer_settime(timer2, 300);
-    fifo8_init(&timerfifo3, 8, timerbuf3);
     timer3 = timer_alloc();
-    timer_init(timer3, &timerfifo3, 1);
+    timer_init(timer3, &timerfifo, 1);
     timer_settime(timer3, 50);
 
     init_keyboard();                            /* キーボードの初期化 */
@@ -82,7 +80,7 @@ void HariMain(void)
         putfonts8_asc_sht(sht_win, 40, 28, COL8_000000, COL8_C6C6C6, s, 10);
 
         io_cli();                                                     /* 外部割り込み禁止（割り込み処理中の割り込み対策） */
-        if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) + fifo8_status(&timerfifo) + fifo8_status(&timerfifo2) + fifo8_status(&timerfifo3) == 0) { /* どちらからもデータが来てないことの確認 */
+        if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) + fifo8_status(&timerfifo) == 0) { /* どちらからもデータが来てないことの確認 */
             io_sti();  /* 外部割り込みの許可と、CPU停止命令一時削除 */
         } else {
             if (fifo8_status(&keyfifo) != 0) {  /* もしキーボードの方のデータが来ていたら */
@@ -128,23 +126,22 @@ void HariMain(void)
             } else if (fifo8_status(&timerfifo) != 0) {
                 i = fifo8_get(&timerfifo);  /* とりあえず読む（空にするため） */
                 io_sti();
-                putfonts8_asc_sht(sht_back, 0, 64, COL8_FFFFFF, COL8_008484, "10[sec]", 7);
-            } else if (fifo8_status(&timerfifo2) != 0) {
-                i = fifo8_get(&timerfifo2);
-                io_sti();
+                if (i == 10) {
+                    putfonts8_asc_sht(sht_back, 0, 64, COL8_FFFFFF, COL8_008484, "10[sec]", 7);
+                } else if (i == 3) {
                 putfonts8_asc_sht(sht_back, 0, 80, COL8_FFFFFF, COL8_008484, "3[sec]", 6);
-            } else if (fifo8_status(&timerfifo3) != 0) {
-                i = fifo8_get(&timerfifo3);
-                io_sti();
-                if (i != 0) {
-                    timer_init(timer3, &timerfifo3, 0); /* 次は0を */
-                    boxfill8(buf_back, binfo->scrnx, COL8_FFFFFF, 8, 96, 15, 111);
                 } else {
-                    timer_init(timer3, &timerfifo3, 1); /* 次は1を */
-                    boxfill8(buf_back, binfo->scrnx, COL8_008484, 8, 96, 15, 111);
+                    /* 0か1 */
+                    if (i != 0) {
+                        timer_init(timer3, &timerfifo, 0); /* 次は0を */
+                        boxfill8(buf_back, binfo->scrnx, COL8_FFFFFF, 8, 96, 15, 111);
+                    } else {
+                        timer_init(timer3, &timerfifo, 1); /* 次は1を */
+                        boxfill8(buf_back, binfo->scrnx, COL8_008484, 8, 96, 15, 111);
+                    }
+                    timer_settime(timer3, 50);
+                    sheet_refresh(sht_back, 8, 96, 16, 112);
                 }
-                timer_settime(timer3, 50);
-                sheet_refresh(sht_back, 8, 96, 16, 112);
             }
         }
     }
