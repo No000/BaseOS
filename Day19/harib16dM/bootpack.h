c9251dd657ffd4a1a2a66d69f679562fb02e5f10
No000 <no000.hima@gmail.com>
2020-02-04 18:50:50 +0900

    ソースの整理（修正箇所）

--- Day19/harib16dM/bootpack.h
+++ Day19/harib16dM/bootpack.h
@@ -239,3 +239,23 @@ struct TASK *task_alloc(void);
 void task_run(struct TASK *task, int level, int priority);
 void task_switch(void);
 void task_sleep(struct TASK *task);
+
+/* window.c */
+void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act);
+void putfonts8_asc_sht(struct SHEET *sht, int x, int y, int c, int b, char *s, int l);  /* 文字出力をまとめる */
+void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c);
+void make_wtitle8(unsigned char *buf, int xsize, char *title, char act);
+
+/* console.c */
+void console_task(struct SHEET *sheet, unsigned int memtotal); /* コンソール用のタスク */
+int cons_newline(int cursor_y, struct SHEET *sheet);
+
+/* file.c */
+struct FILEINFO {
+    unsigned char name[8], ext[3], type; /* name：Fileネーム、ext：拡張子、type：ファイル情報 */
+    char reserve[10];   /* 予約領域 */
+    unsigned short time, date, clustno; /* time：時刻、date：日付、clustno：セクタ番号*/
+    unsigned int size;  /* Fileサイズ */
+};
+void file_readfat(int *fat, unsigned char *img);
+void file_loadfile(int clustno, int size, char *buf, int *fat, char *img);
