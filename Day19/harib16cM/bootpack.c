ae87a6ab2d436f2198c2916dc70c1e32a60f4158
No000 <no000.hima@gmail.com>
2020-02-04 18:35:04 +0900

    FATに対応（修正箇所）

--- Day19/harib16cM/bootpack.c
+++ Day19/harib16cM/bootpack.c
@@ -620,31 +620,31 @@ int cons_newline(int cursor_y, struct SHEET *sheet)
 void file_readfat(int *fat, unsigned char *img)
 /* ディスクイメージ内のFATの圧縮をとく */
 {
-    int i, j = 0;
-    for (i = 0; i < 2880; i += 2) {
-        fat[i + 0] = (img[j + 0]      | img[j + 1] << 8) & 0xfff;
-        fat[i + 1] = (img[j + 1] >> 4 | img[j + 2] << 4) & 0xfff;
-        j += 3;
+    int i, j = 0;   /* カウンタ */
+    for (i = 0; i < 2880; i += 2) { /* 2880=全セクタ数 */
+        fat[i + 0] = (img[j + 0]      | img[j + 1] << 8) & 0xfff;   /* データを並べ直し */
+        fat[i + 1] = (img[j + 1] >> 4 | img[j + 2] << 4) & 0xfff;   /* 並べ方は書籍P388を参照 */
+        j += 3; /* 圧縮される側のデータのカウンタを進める */
     }
     return;
 }
 
 void file_loadfile(int clustno, int size, char *buf, int *fat, char *img)
-{
+{   /* 引数の情報を元にメモリ上に正しく並べる */
     int i;
     for (;;) {
-        if (size <= 512) {
-            for (i = 0; i < size; i++) {
+        if (size <= 512) {  /* 最後のセクタのロード */
+            for (i = 0; i < size; i++) {    /* データをロード */
                 buf[i] = img[clustno * 512 + i];
             }
-            break;
+            break;  /* 無限ループから出る */
         }
-        for (i = 0; i < 512; i++) {
+        for (i = 0; i < 512; i++) { /* 2以上のセクタのロード */
             buf[i] = img[clustno * 512 + i];
         }
         size -= 512;
         buf += 512;
-        clustno = fat[clustno];
+        clustno = fat[clustno]; /* アドレスを私てる */
     }
     return;
 }
