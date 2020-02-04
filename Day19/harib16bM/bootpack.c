b40a8f2ce303ab00278935573a46102c0de26375
No000 <no000.hima@gmail.com>
2020-02-04 16:46:56 +0900

    typeコマンド改良（修正箇所）

--- Day19/harib16bM/bootpack.c
+++ Day19/harib16bM/bootpack.c
@@ -484,8 +484,7 @@ void console_task(struct SHEET *sheet, unsigned int memtotal)
                             }
                         }
                         cursor_y = cons_newline(cursor_y, sheet);
-                    } else if (cmdline[0] == 't' && cmdline[1] == 'y' && cmdline[2] == 'p' &&
-                        cmdline[3] == 'e' && cmdline[4] == ' ') {
+                    } else if (strncmp(cmdline, "type ", 5) == 0) {   /* 5で比較する文字数指定 */
                             /* typeコマンド */
                             /* ファイル名を準備する */
                             for (y = 0; y < 11; y++) {
@@ -529,11 +528,30 @@ void console_task(struct SHEET *sheet, unsigned int memtotal)
                                 /* 1文字ずつ出力 */
                                 s[0] = p[x];    /* 1文字ずつ入れる */
                                 s[1] = 0;
-                                putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, s, 1);
-                                cursor_x += 8;  /* 一文字カーソルを進める */
-                                if (cursor_x == 8 + 240) {  /* カーソルの位置がコンソールの端まで来たら */
-                                    cursor_x = 8;   /* 元に戻す */
-                                    cursor_y = cons_newline(cursor_y, sheet);   /* 次の行に移動 */
+                                if (s[0] == 0x09) { /* タブ */
+                                    for(;;) {
+                                        putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, " ", 1);
+                                        cursor_x += 8;
+                                        if (cursor_x == 8 + 240) {
+                                            cursor_x = 8;
+                                            cursor_y = cons_newline(cursor_y, sheet);
+                                        }
+                                        if (((cursor_x - 8) & 0x1f) == 0) { /* -8はコンソール8ドット分 */
+                                            break;  /* 32で割り切れたらbreak */
+                                        }
+                                    }
+                                } else if (s[0] == 0x0a) {  /* 改行 */
+                                    cursor_x = 8;
+                                    cursor_y = cons_newline(cursor_y, sheet);
+                                } else if (s[0] == 0x0d) {  /* 復帰 */
+                                    /* とりあえずなにもしない */
+                                } else {    /* 普通の文字 */
+                                    putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, s, 1);
+                                    cursor_x += 8;  /* 一文字カーソルを進める */
+                                    if (cursor_x == 8 + 240) {  /* カーソルの位置がコンソールの端まで来たら */
+                                        cursor_x = 8;   /* 元に戻す */
+                                        cursor_y = cons_newline(cursor_y, sheet);   /* 次の行に移動 */
+                                    }
                                 }
                             }
                         } else {    /* ファイルがなかった時のエラー処理 */
