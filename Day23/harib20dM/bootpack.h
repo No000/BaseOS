3b00e5a312d27f8e96bf7d451e95e39ec8ab564e
No000 <no000.hima@gmail.com>
2020-02-11 18:09:01 +0900

    線を引く（修正箇所）

--- Day23/harib20dM/bootpack.h
+++ Day23/harib20dM/bootpack.h
@@ -271,6 +271,7 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline);
 int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int eax); /* 機能番号対応文字表示API */
 int *inthandler0d(int *esp);
 int *inthandler0c(int *esp);
+void hrb_api_linewin(struct SHEET *sht, int x0, int y0, int x1, int y1, int col);
 
 /* file.c */
 struct FILEINFO {
