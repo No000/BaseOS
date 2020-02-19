c755c5ff31e3593a716f457e2d0c2c3fb7cf209c
No000 <no000.hima@gmail.com>
2020-02-19 17:00:43 +0900

    アプリケーションを守ろう(1)

---
+++ Day27/harib24cM/file.c
@@ -0,0 +1,74 @@
+/* ファイル関連 */
+
+#include "bootpack.h"
+
+void file_readfat(int *fat, unsigned char *img)
+/* ディスクイメージ内のFATの圧縮をとく */
+{
+    int i, j = 0;   /* カウンタ */
+    for (i = 0; i < 2880; i += 2) { /* 2880=全セクタ数 */
+        fat[i + 0] = (img[j + 0]      | img[j + 1] << 8) & 0xfff;   /* データを並べ直し */
+        fat[i + 1] = (img[j + 1] >> 4 | img[j + 2] << 4) & 0xfff;   /* 並べ方は書籍P388を参照 */
+        j += 3; /* 圧縮される側のデータのカウンタを進める */
+    }
+    return;
+}
+
+void file_loadfile(int clustno, int size, char *buf, int *fat, char *img)
+{   /* 引数の情報を元にメモリ上に正しく並べる */
+    int i;
+    for (;;) {
+        if (size <= 512) {  /* 最後のセクタのロード */
+            for (i = 0; i < size; i++) {    /* データをロード */
+                buf[i] = img[clustno * 512 + i];
+            }
+            break;  /* 無限ループから出る */
+        }
+        for (i = 0; i < 512; i++) { /* 2以上のセクタのロード */
+            buf[i] = img[clustno * 512 + i];
+        }
+        size -= 512;
+        buf += 512;
+        clustno = fat[clustno]; /* アドレスを私てる */
+    }
+    return;
+}
+
+struct FILEINFO *file_search(char *name, struct FILEINFO *finfo, int max)
+{
+    int i, j;
+    char s[12];
+    for (j = 0; j < 11; j++) {
+        s[j] = ' ';
+    }
+    j = 0;
+    for (i = 0; name[i] != 0; i++) {
+        if (j >= 11) { return 0; /* 見つからなかった */ }
+        if (name[i] == '.' && j <= 8) {
+            j = 8;
+        } else {
+            s[j] = name[i];
+            if ('a' <= s[j] && s[j] <= 'z') {
+                /* 小文字は大文字に直す */
+                s[j] -= 0x20;
+            }
+            j++;
+        }
+    }
+    for (i = 0; i < max; ) {
+        if (finfo->name[0] == 0x00) {
+            break;
+        }
+        if ((finfo[i].type & 0x18) == 0) {
+            for (j = 0; j < 11; j++) {
+                if (finfo[i].name[j] != s[j]) {
+                    goto next;
+                }
+            }
+            return finfo + i; /* ファイルが見つかった */
+        }
+next:
+        i++;
+    }
+    return 0; /* 見つからなかった */
+}
