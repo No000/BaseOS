6511f661fb5a9462ae7081c5f31034a4e48a22be
No000 <no000.hima@gmail.com>
2020-02-24 22:31:13 +0900

    インベーダーム

---
+++ Day29/harib26eM/clib/printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <apilib.h>
+
+int printf(char format, ...)
+{
+  va_list ap;
+  char s[1000];
+  int i;
+
+  va_start(ap, format);
+  i = vsprintf(s, format, ap);
+  api_putstr0(s);
+  va_end(ap);
+  return i;
+}
